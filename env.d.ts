5625f00a21288a9509d1f50dde7ca57fa2245eb2
shenhuanjie <shenhuanjie@live.cn>
2022-10-21 00:42:27 +0800

    feature: 格式化代码

--- env.d.ts
+++ env.d.ts
@@ -1,10 +1,1 @@
-/*
- * @Author: shenhuanjie@live.cn
- * @Date: 2022-10-20 00:22:48
- * @LastEditors: shenhuanjie@live.cn
- * @LastEditTime: 2022-10-20 00:46:25
- * @FilePath: \env.d.ts
- * @Description: 环境变量
- *
- */
-/// <reference types="vite/client" />
+/// <reference types="vite/client.d.ts" />
